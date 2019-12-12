be9fc47e7964361eb246998c10d17449b18415b4
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-12 14:57:27 -0500

    Update CSS & scroll

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
  * Define Global Variables
  *
 */
-const navbar = document.getElementById('navbar_list');
+const navbar = document.getElementById('navbar__list');
 const menu = document.getElementsByClassName('menu__link');
 const sections = document.getElementsByTagName('section');
 
@@ -26,13 +26,6 @@ const sections = document.getElementsByTagName('section');
  * Start Helper Functions
  *
 */
-function showActive(section) {
-    section.classList.add('your-active-class');
-}
-
-function removeActive(section) {
-    section.classList.remove('your-active-class');
-}
 
 
 /**
@@ -44,6 +37,47 @@ function removeActive(section) {
 function navMenu(section) {
     return document.createTextNode(section.getAttribute('data-nav'));
 }
+
+//add the links to the nav list
+function addLink(links, id) {
+    const listItem = document.createElement('li');
+    listItem.appendChild(links);
+
+    listItem.classList.add('menu__link');
+
+    //use click to scroll to section
+    links.addEventListener('click', function() {
+        document.getElementById(id).scrollIntoView(
+            {behavior: 'smooth'} );
+    });
+
+    navbar.appendChild(listItem);
+}
+
+
+function removeActive(section) {
+    for(const section of sections) {
+    section.classList.remove('your-active-class');
+}
+}
+
+function showActive(section) {
+    section.classList.add('your-active-class');
+}
+
+//Check to see is section is in viewport. jQuery function help from Stephen Irving here: https://coderwall.com/p/fnvjvg/jquery-test-if-element-is-in-viewport
+
+const isInView = (element) => {
+    const w = window,
+    elBounds = element.getBoundingClientRect(),
+    scrollTop = w.pageYOffset,
+    elTop = elBounds.y + scrollTop;
+
+    return(
+        elTop < (w.innerHeight + scrollTop) &&
+        elTop > (scrollTop - elBounds.height));
+}
+
 // build the nav
 for (const section of sections) {
 //create the a links
@@ -52,11 +86,20 @@ for (const section of sections) {
     const navLinks = navMenu(section);
 
     const id = section.id
-    links.appendChild(navLinks, id);
+    links.appendChild(navLinks);
+    addLink(links, id);
 }
 
 // Add class 'active' to section when near top of viewport
-
+window.addEventListener('scroll', function() {
+    for (const section of sections) {
+        if (isInView(section)) {
+            console.log(section);
+            removeActive();
+            showActive();
+        }
+    }
+});
 
 // Scroll to anchor ID using scrollTO event
 
